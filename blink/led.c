09b1e6e2bb905b677a3e7b824417a2e4e11380fc
drew york <119648808+dre-yo@users.noreply.github.com>
2024-08-26 15:23:28 -0700

    changed code to not use arduino pre defined variables

--- blink/led.c
+++ blink/led.c
@@ -1,16 +1,38 @@
 #include <avr/io.h>
+#include <inttypes.h>
 #include <util/delay.h>
 
+typedef uint8_t byte;
+
 int main(void) {
-  DDRB = DDRB | (1 << DDB5);
+  DDRB = 32;
 
   while (1) {
-    PORTB = PORTB | (1 << PORTB5);
+    volatile byte *portB = 0x25;
 
-    _delay_ms(1000);
+    *portB = 32;
+    for (long i = 0; i < 2000000; i++) {
+      *portB = 32;
+    }
 
-    PORTB = PORTB & ~(1 << PORTB5);
-
-    _delay_ms(1000);
+    *portB = 0;
+    for (long i = 0; i < 1000000; i++) {
+      *portB = 0;
+    }
   }
 }
+
+// old code
+// int main(void) {
+//   DDRB = DDRB | (1 << DDB5);
+
+//   while (1) {
+//     PORTB = PORTB | (1 << PORTB5);
+
+//     _delay_ms(1000);
+
+//     PORTB = PORTB & ~(1 << PORTB5);
+
+//     _delay_ms(1000);
+//   }
+// }
