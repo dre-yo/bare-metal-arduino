d3e78d5a93afb3b36abac9817f0debf6f9a93ce2
drew york <119648808+dre-yo@users.noreply.github.com>
2024-08-26 15:48:24 -0700

    removed old code and changed naming for better readability

--- blink/led.c
+++ blink/led.c
@@ -1,39 +1,22 @@
-#include <avr/io.h>
 #include <inttypes.h>
-#include <util/delay.h>
 
 typedef uint8_t byte;
 
-#define MYPORTB *((volatile byte *)0x25)
-#define MYDDRB *((volatile byte *)0x24)
+#define PORTB *((volatile byte *)0x25)
+#define DDRB *((volatile byte *)0x24)
 
 int main(void) {
-  MYDDRB = 32;
+  DDRB = 32;
 
   while (1) {
-    MYPORTB = 32;
+    PORTB = 32;
     for (long i = 0; i < 3000000; i++) {
-      MYPORTB = 32;
+      PORTB = 32;
     }
 
-    MYPORTB = 0;
+    PORTB = 0;
     for (long i = 0; i < 1000000; i++) {
-      MYPORTB = 0;
+      PORTB = 0;
     }
   }
 }
-
-// old code
-// int main(void) {
-//   DDRB = DDRB | (1 << DDB5);
-
-//   while (1) {
-//     PORTB = PORTB | (1 << PORTB5);
-
-//     _delay_ms(1000);
-
-//     PORTB = PORTB & ~(1 << PORTB5);
-
-//     _delay_ms(1000);
-//   }
-// }
