fb0e08db0675173184eab681d779471d8fbecb2a
drew york <119648808+dre-yo@users.noreply.github.com>
2024-08-28 00:20:13 -0700

    changes to makefile usb port and changed code to use bit shifting

--- blink/led.c
+++ blink/led.c
@@ -6,17 +6,17 @@ typedef uint8_t byte;
 #define DDRB *((volatile byte *)0x24)
 
 int main(void) {
-  DDRB = 32;
+  DDRB |= (1 << 5);
 
   while (1) {
-    PORTB = 32;
+    PORTB ^= (1 << 5);
     for (long i = 0; i < 3000000; i++) {
-      PORTB = 32;
+      PORTB |= (1 << 5);
     }
 
-    PORTB = 0;
+    PORTB ^= (1 << 5);
     for (long i = 0; i < 1000000; i++) {
-      PORTB = 0;
+      PORTB &= ~(1 << 5);
     }
   }
 }
